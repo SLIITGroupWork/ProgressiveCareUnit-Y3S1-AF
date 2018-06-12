f534b59f67ef3afa4f10814018e0c0e1f841671b
Chathuranga Basnayake <chathurangabasnayake@outlook.com>
2018-06-12 21:45:54 +0530

    Delete Routes.js

--- Backend/NodeServices/Routes.js
+++
@@ -1,8 +0,0 @@
-const express = require('express');
-const routes = express.Router();
-
-const patientRegistrationRoutes = require('./src/controller-routers/patient-registration.routes');
-
-routes.use('/patient-registration', patientRegistrationRoutes);
-
-module.exports = routes;

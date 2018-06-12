b00eba16eb8250f94b3cc8d2c7ce378c0cc0ee48
Chathuranga Basnayake <chathurangabasnayake@outlook.com>
2018-06-13 00:38:43 +0530

    Response updated. _id,  __v removed. id added to reponse

--- Backend/NodeServices/src/data-trans-objects/response.js
+++ Backend/NodeServices/src/data-trans-objects/response.js
@@ -12,10 +12,31 @@ const createResponse = (status, isSuccess, data = [], message = null) => {
         data = [data];
     }
 
+    let newData = [];
+
+    for (let ind = 0, len = data.length; ind < len; ind++) {
+
+        let tempData = data[ind]._doc;
+
+        if (tempData) {
+            tempData.id = tempData._id;
+            delete tempData._id;
+            delete tempData.__v;
+        }
+        else {
+            tempData = data[ind];
+            tempData.id = tempData._id;
+            tempData._id = undefined;
+            tempData.__v = undefined;
+        }
+
+        newData.push(tempData);
+    }
+
     return {
         status: status,
         isSuccess: isSuccess,
-        data: data,
+        data: newData,
         message: message,
         respondDateTime: new Date()
     };

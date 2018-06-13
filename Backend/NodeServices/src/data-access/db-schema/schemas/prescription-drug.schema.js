5b240e432fce91da0fba367d1d99325a0258131e
it16153950 <it16153950@my.sliit.lk>
2018-06-14 04:42:50 +0530

    Commi modifications

--- Backend/NodeServices/src/data-access/db-schema/schemas/prescription-drug.schema.js
+++ Backend/NodeServices/src/data-access/db-schema/schemas/prescription-drug.schema.js
@@ -2,12 +2,12 @@ const mongoose  = require('mongoose');
 const Schema = mongoose.Schema;
 
 const prescriptionDrugsSchema = new Schema({
-    regId:{
+    regId:[{
         type:Schema.Types.ObjectId,
         ref:tableNames.PatientRegistrationSchema,
         require:true
 
-    },
+    }],
     presID: [{
         type: Schema.Types.ObjectId,
         ref: tableNames.presciptionSchema,

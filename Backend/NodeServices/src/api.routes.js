544916fc4b983668452eeb206707a5b7d7ea8711
Harin Wijesekara <harinwijesekara@gmail.com>
2018-06-15 07:59:28 +0530

    Implement the doctor patient controllers and routers

--- Backend/NodeServices/src/api.routes.js
+++ Backend/NodeServices/src/api.routes.js
@@ -4,7 +4,13 @@ const router = express.Router();
 const patientRegistrationsRoutes = require('./controller-routers/patient-registration.routes');
 const usersRoutes = require('./controller-routers/user.routes');
 
+const doctor = require('./controller-routers/doctor.routes');
+const doctorPatient = require('./controller-routers/doctor-patient.routes');
+
 router.use('/patient-registrations', patientRegistrationsRoutes);
 router.use('/users', usersRoutes);
+
+router.use('./doctor', doctor);
+router.use('/doctor-patient', doctorPatient);
 
 module.exports = router;

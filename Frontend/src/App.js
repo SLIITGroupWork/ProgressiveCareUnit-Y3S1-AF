e6ba5a90aa153c1e1bcbb65850e3b5f440f17845
Chathuranga Basnayake <chathurangabasnayake@outlook.com>
2018-06-15 18:45:06 +0530

    Added a Nav bar and footer

--- Frontend/src/App.js
+++ Frontend/src/App.js
@@ -1,13 +1,16 @@
 import React, { Component } from 'react';
-import '../node_modules/bootstrap/dist/css/bootstrap.min.css';
-import '../node_modules/font-awesome/css/font-awesome.min.css';
 import './App.css';
+
+import Navbar from './components/layouts/Navbar';
+import Footer from './components/layouts/Footer';
 
 class App extends Component {
     render() {
         return (
             <div className="App">
+                <Navbar />
                 <h1>My React App</h1>
+                <Footer />
             </div>
         );
     }

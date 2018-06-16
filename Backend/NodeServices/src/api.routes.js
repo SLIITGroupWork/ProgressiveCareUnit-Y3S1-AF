7fd2b5ac9af2ad9d44c60ee7bedadad7344cf63e
Chathuranga Basnayake <chathurangabasnayake@outlook.com>
2018-06-16 13:55:25 +0530

    Merge conflicts in api routes

--- Backend/NodeServices/src/api.routes.js
+++ Backend/NodeServices/src/api.routes.js
@@ -10,17 +10,10 @@ const drugRoutes = require('./controller-routers/drug.routes');
 const prescriptionRoutes = require('./controller-routers/prescription.route');
 const billRoutes = require('./controller-routers/bill.route');
 const billGenerateRoutes = require('./controller-routers/billGenerate.route');
-
-
-<<<<<<< HEAD
-
-
 const patientAllergiesRoutes= require('./controller-routers/patient-allergies.routes');
 const patientPhysicalExamsRoutes= require('./controller-routers/patient-physical-exams.routes');
-// Riutes that don't need secured
-=======
+
 // Routes that don't need secured
->>>>>>> 935dad50262cfc2ea77c9bf7eb795e694485c1a8
 router.use('/authorizations', authorizationsRoutes);
 
 // Secured routes
@@ -30,22 +23,13 @@ router.use('/users', passport.authenticate('jwt', { session: false }), usersRout
 router.use('./doctor', doctor);
 router.use('/doctor-patient', doctorPatient);
 
-<<<<<<< HEAD
-router.use('/drugs', drugRoutes);
-router.use('/prescriptions', prescriptionRoutes);
-router.use('/bill', billRoutes);
-
 router.use('/patient-allergies',patientAllergiesRoutes);
 router.use('/patient-physicalExams',patientPhysicalExamsRoutes);
 
-=======
 //router.use('/drugs',passport.authenticate('jwt', { session: false }), drugRoutes);
 router.use('/drugs',drugRoutes);
 router.use('/prescriptions',passport.authenticate('jwt', { session: false }), prescriptionRoutes);
 router.use('/bill',passport.authenticate('jwt', { session: false }), billRoutes);
 router.use('/billPDF',passport.authenticate('jwt', { session: false }), billGenerateRoutes);
-
-
->>>>>>> 935dad50262cfc2ea77c9bf7eb795e694485c1a8
 
 module.exports = router;
